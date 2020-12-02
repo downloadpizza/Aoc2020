466ce7400c054c5c1a81482bb0866165d0271615
gabriel <klenngabriel@gmail.com>
2020-12-02 06:30:14 +0100

    Add Day 2

---
+++ src/main/kotlin/day2/Day2.kt
@@ -0,0 +1,56 @@
+package day2
+
+import utils.NoSolutionFoundException
+import utils.getResourceAsLines
+
+val input = getResourceAsLines("/day2/input.txt")
+
+data class PasswordPolicy(val min: Int, val max: Int, val char: Char) {
+    fun matches(password: String): Boolean {
+        val a = password.count { it -> it == char }
+        return a in min..max
+    }
+
+    fun matches2(password: String): Boolean {
+        val minc = password.getOrNull(min - 1) ?: return false
+        val maxc = password.getOrNull(max - 1) ?: return false
+
+
+        return (minc == char) xor (maxc == char)
+    }
+}
+
+fun main() {
+//    println(task1())
+    println(task2())
+}
+
+fun check1(line: String): Boolean {
+    val (nums, char, pw) = line.split(" ")
+    val (min, max) = nums.split("-").map(String::toInt)
+    return PasswordPolicy(min, max, char[0]).matches(pw)
+}
+
+fun check2(line: String): Boolean {
+    val (nums, chars, pw) = line.split(" ")
+    val (min, max) = nums.split("-").map(String::toInt)
+    val char = chars[0]
+    return PasswordPolicy(min, max, char).matches2(pw.trim())
+}
+
+fun task1(): Int {
+    return input.count {
+        check1(it)
+    }
+    throw NoSolutionFoundException()
+}
+fun task2(): Int {
+    val pp2 = PasswordPolicy(1, 3, 'a')
+
+    print(pp2.matches2("abcde"))
+
+    return input.count {
+        check2(it)
+    }
+    throw NoSolutionFoundException()
+}
