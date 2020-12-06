67e61fc598dbe973ea9e788cad3be404b8e3820f
gabriel <klenngabriel@gmail.com>
2020-12-06 12:34:28 +0100

    Change D5-2 to expression body

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -16,9 +16,7 @@ fun id(s: String): Int = s.replace("[BR]".toRegex(), "1")
 fun task1(): Int = input
         .maxOrNull()!!
 
-fun task2(): Int {
-    return (0..(127*8+7))
-            .dropWhile { it !in input }
-            .dropWhile { it in input }
-            .first()
-}
+fun task2(): Int = (0..(127*8+7))
+        .dropWhile { it !in input }
+        .dropWhile { it in input }
+        .first()
